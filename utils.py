f7b77eed4602219abc59e01c5a10f4857b543796
Mariia Reznichenko <reznichenko.masha@gmail.com>
2026-03-02 13:13:19 +0200

    Added isPrime function to utils.py

--- utils.py
+++ utils.py
@@ -1,2 +1,14 @@
 def is_power_of_two(n: int) -> bool:
     return n > 0 and (n & (n - 1)) == 0
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
